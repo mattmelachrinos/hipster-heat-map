8f5eb4c700b40b9aa48aaa25c9baa6e3bd584a76
Matt <mmelach@gmail.com>
2015-05-03 17:03:36 -0400

    handling JSON objects

--- Places_API_Caller.py
+++ Places_API_Caller.py
@@ -8,15 +8,27 @@ with open('keywords.txt', 'r') as f:
 	with open('output_locations.txt', 'w') as output_file:	
 		for word in f:
 			keyword = word
+			for y in range()
 			url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +  keyword + "&"  +"key=" + key
-			print url
+		
+			print keyword
 			response = requests.get(url).text
 			output = json.loads(response)
-			output_file.write(str(output))
+		#	print response
+			#output_file.write(json.dumps(output))
+
+			for result in output['results']:
+				output_file.write(str(result['geometry']['location']['lat']) + " , " + str(result['geometry']['location']['lng']) + "\n")
+			
+
+
+
+
+"""
 			try:
-				output_file.write(output['results'])
+				output_file.write(output)
 			except UnicodeEncodeError:
 				print "boooooo unicodes suxxxxx"
 
 
-
+"""
